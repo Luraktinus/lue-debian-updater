c7c4e15c7509d9ca1862ecd5a0df8e15b503c79a
Sir_Boops <admin@boops.me>
2018-03-09 06:44:43 -0700

    Use functions

--- src/ldu.sh
+++ src/ldu.sh
@@ -1,23 +1,57 @@
 #!/bin/sh
 
+function clean () {
+	if [ "$DISTRO" == "debian" ]; then
+		sudo apt clean
+	fi
+}
+
+
+function autoremove () {
+	if [ "$DISTRO" == "debian" ]; then
+		yes | sudo apt autoremove
+	fi
+}
+
+function upgrade () {
+	if [ "$DISTRO" == "debian" ]; then
+		yes | sudo apt upgrade
+	fi
+}
+
+function update () {
+	if [ "$DISTRO" == "debian" ]; then
+		sudo apt update
+	fi
+}
+
+function checkPkgMgr () {
+	apt
+	if [ $? == 0 ]; then
+		DISTRO=debian
+	fi
+}
+
+
 # Pre-running sudo to run as root (to give a nicer ui flow) and init a clear screen
 sudo echo ""
+checkPkgMgr
 clear
 
 #--- Greeting
 echo "+------------------------+"
-echo "|Lue Debian Updater 1.2.2|"
+echo "|Lue Distro Updater 1.2.2|"
 echo "+------------------------+"
 echo "I will take care of your system and automatically agree to promts."
 echo ""
 sleep 3
 clear
 
-
 #--- Update downloads the list of packages 
-echo "running apt update... (1/4)"
+echo "Running syncing repos... (1/4)"
 echo ""
-sudo apt update
+update
+exit 0
 echo ""
 #---
 
@@ -25,21 +59,21 @@ echo ""
 #--- I use echo "y" | to automatically agree to the promt
 echo "running apt upgrade... (2/4)"
 echo ""
-echo "y" | sudo apt upgrade 
+upgrade
 echo ""
 #---
 
 #--- Autoremove removes unneeded packages
 echo "running apt autoremove... (3/4)"
 echo ""
-echo "y" | sudo apt autoremove
+autoremove
 echo ""
 #---
 
 #--- Removes cached packages
 echo "cleaning up... (4/4)"
 echo ""
-sudo apt clean
+clean
 echo ""
 sleep 1
 clear
