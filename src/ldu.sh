f3a29ea18405abc9ad3d4f9e1e66c578d5915c12
Lue <lue@acer-deb3.local>
2018-03-13 23:56:26 +0100

    Added Arch support + Fixes for new version

--- src/ldu.sh
+++ src/ldu.sh
@@ -1,105 +1,128 @@
-#!/bin/bash
-
-function clean () {
-	if [ "$DISTRO" == "debian" ]; then
+#!/bin/sh
+clean () {
+	# Messages
+	echo "Cleaning up the local cache... (4/4)"
+	echo ""
+	# ---
+	if test "$DISTRO" = "Debian"
+	then
 		sudo apt clean
 	fi
 
-	if [ "$DISTRO" == "arch" ]; then
+	if test "$DISTRO" = "Arch"
+	then
 		yes | sudo pacman -Sc
 	fi
+	echo ""
 }
 
 
-function autoremove () {
-	if [ "$DISTRO" == "debian" ]; then
+autoremove () {
+	# Messages
+	echo "Removing unneeded deps... (3/4)"
+	echo ""
+	# ---
+	if test "$DISTRO" = "Debian"
+	then
 		yes | sudo apt autoremove
 	fi
 
-	if [ "$DISTRO" == "arch" ]; then
+	if test "$DISTRO" = "Arch" 
+	then
 		yes | sudo pacman -Rns $(pacman -Qdtq)
 	fi
+	echo ""
 }
 
-function upgrade () {
-	if [ "$DISTRO" == "debian" ]; then
+upgrade () {
+	# Messages
+	echo "Upgrading the system... (2/4)"
+	echo ""
+	# ---
+	if test "$DISTRO" = "Debian" 
+	then
+		echo "upgrade debian"
 		yes | sudo apt upgrade
 	fi
 
-	if [ "$DISTRO" == "arch" ]; then
+	if test "$DISTRO" = "Arch" 
+	then
 		yes | sudo pacman -Syu
 	fi
+	echo ""
 }
 
-function update () {
-	if [ "$DISTRO" == "debian" ]; then
+update () {
+	# Messages
+	echo "Syncing repos... (1/4)"
+	echo ""
+	# ---
+	
+	if test "$DISTRO" = "Debian" 
+	then
 		sudo apt update
 	fi
 
-	if [ "$DISTRO" == "arch" ]; then
+	if test "$DISTRO" = "Arch" 
+	then
 		sudo pacman -Syy
 	fi
+	echo ""
 }
 
-function checkPkgMgr () {
-	apt
-	if [ $? == 0 ]; then
-		DISTRO=debian
+checkPkgMgr () {
+	# Messages
+	echo "Checking what distribution you use..."
+	echo ""
+	# ---
+	
+	echo "Detected Package manager:"
+	if command -v apt
+	then
+		DISTRO=Debian
 	fi
 
-	pacman
-	if [ $? == 0 ]; then
-		DISTRO=arch
+	if command -v pacman
+	then
+		DISTRO=Arch
 	fi
+	echo ""
+	echo "You use $DISTRO"
+	echo ""
 }
 
 
 # Pre-running sudo to run as root (to give a nicer ui flow) and init a clear screen
 sudo echo ""
-checkPkgMgr
 clear
 
-#--- Greeting
+#--- Greeting and initializing
 echo "+------------------------+"
 echo "|Lue Distro Updater 1.2.2|"
 echo "+------------------------+"
 echo "I will take care of your system and automatically agree to promts."
 echo ""
+checkPkgMgr
 sleep 3
 clear
 
 #--- Update downloads the list of packages 
-echo "Syncing repos... (1/4)"
-echo ""
 update
-exit 0
-echo ""
-#---
 
 #--- Upgrade installs new versions
 #--- I use echo "y" | to automatically agree to the promt
-echo "Upgrading the system... (2/4)"
-echo ""
 upgrade
-echo ""
-#---
 
 #--- Autoremove removes unneeded packages
-echo "Removing unneeded deps... (3/4)"
-echo ""
 autoremove
-echo ""
-#---
 
 #--- Removes cached packages
-echo "Cleaning up the local cache... (4/4)"
-echo ""
 clean
-echo ""
+
+#--- Endmessage
 sleep 1
 clear
 
-#--- Endmessage
 echo "+--------------------------+"
 echo "|Your system is up-to-date!|"
 echo "+--------------------------+"
